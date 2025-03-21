c0a7d43fe9db1ff03b077a2b5291143c6a53e92d
unknown <3160189098@qq.com>
2025-03-21 22:24:36 +0800

    Add MultipleThreeNums.py

---
+++ MultipleThreeNums.py
@@ -0,0 +1,6 @@
+x=int(input("Please enter the first number: "))
+y=int(input("Please enter the second number: "))
+z=int(input("Please enter the third number: "))
+res=x*y*z
+print("The result is:")
+print(res)
